b132004d27ee092606967a8798ea5ff46401bbe3
bigmike68 <57294099+bigmike68@users.noreply.github.com>
2019-11-02 13:33:32 -0500

    Data and Methods
    Using View Data and Methods

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 new Vue({
   el: '#vue-app',
   data: {
-    name: 'Big Mike'
+    name: 'Big Mike',
+    job: 'Web Developer',
+    team: 'Golden State Warriors'
+  },
+  methods: {
+    greet: function(person){
+      return 'Hola' + person;
+    }
   }
 });
