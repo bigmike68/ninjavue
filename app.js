42832eb90c73933fd8b233f043fcbf7e6915f56a
bigmike68 <57294099+bigmike68@users.noreply.github.com>
2019-11-02 14:20:07 -0500

    Data Binding
    Data binding to different attributes using v-bind: and v-html in Vue.

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ new Vue({
   data: {
     name: 'Glenny',
     job: 'Web Developer',
-    team: 'Golden State Warriors'
+    team: 'Golden State Warriors',
+    website: 'https://www.si.com/',
+    website2: 'https://www.espn.com/',
+    websiteTag: '<a href="https://www.espn.com/">ESPN Online</a>'
   },
   methods: {
     greet: function(time){
